09125e3468f5f3e587559811dbf9ad890d49cdc1
ShyamHari88 <shyamhari88@users.noreply.github.com>
2026-02-11 23:09:03 +0530

    fix default export for connectDB

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,12 +1,14 @@
+import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
-    console.log("MONGO FROM RENDER =", process.env.MONGODB_URI);
-
-    await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI);
     console.log("MongoDB connected");
   } catch (error) {
     console.error("MongoDB connection error:", error.message);
     process.exit(1);
   }
 };
+ 
 
+export default connectDB; 
